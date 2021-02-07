f2f97e455d91309cfb7c40d13b3b78172ed3f13d
Vladislav Tupikin <mrrefactoring@yandex.ru>
2021-02-07 14:37:05 +0300

    sendRequest implementation added

--- src/clients/baseClient.ts
+++ src/clients/baseClient.ts
@@ -1,21 +1,54 @@
+import axios, { AxiosInstance } from 'axios';
 import { Client } from './client';
 import { Callback, RequestConfig } from '../types';
 import { Config } from '../config';
 
 export class BaseClient implements Client {
-  constructor(private config: Config) {}
+  private instance: AxiosInstance;
+
+  constructor(private config: Config) {
+    this.instance = axios.create({
+      ...config.baseRequestConfig,
+      baseURL: 'https://api.trello.com/1',
+    });
+  }
 
   async sendRequest<T>(requestConfig: RequestConfig, callback?: Callback<T> | undefined, telemetryData?: Partial<any>): Promise<T>;
   async sendRequest<T>(requestConfig: RequestConfig, callback: Callback<T>, telemetryData?: Partial<any>): Promise<void>;
   async sendRequest<T>(rawRequestConfig: RequestConfig, callback?: Callback<T>, telemetryData?: Partial<any>): Promise<T | void> {
-    const requestConfig = {
+    const requestConfig: RequestConfig = {
       ...rawRequestConfig,
-      key: this.config.apiKey,
-      token: this.config.apiToken,
+      params: {
+        ...rawRequestConfig.params,
+        key: this.config.apiKey,
+        token: this.config.apiToken,
+      },
     };
 
-    console.log(requestConfig, callback, telemetryData);
+    try {
+      const response = await this.instance.request(requestConfig);
 
-    return Promise.resolve();
+      const callbackResponseHandler = callback && ((data: T): void => callback(null, data));
+      const defaultResponseHandler = (data: T): T => data;
+
+      const responseHandler = callbackResponseHandler ?? defaultResponseHandler;
+
+      this.config.middlewares?.onResponse?.(response.data);
+
+      return responseHandler(response.data);
+    } catch (e) {
+      const callbackErrorHandler = callback && ((error: Config.Error) => callback(error));
+      const defaultErrorHandler = (error: Error) => {
+        throw error;
+      };
+
+      const errorHandler = callbackErrorHandler ?? defaultErrorHandler;
+
+      this.config.middlewares?.onError?.(e);
+
+      return errorHandler(e);
+    } finally {
+      console.log(telemetryData);
+    }
   }
 }
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,23 @@
+import { AxiosError, AxiosRequestConfig } from 'axios';
+
 export interface Config {
   apiKey: string;
   apiToken: string;
+  baseRequestConfig?: Config.BaseRequestConfig;
+  middlewares?: Config.Middlewares;
+}
+
+export namespace Config {
+  export type Error = AxiosError;
+  export type BaseRequestConfig = AxiosRequestConfig;
+
+  export interface Middlewares {
+    onError?: Config.Middlewares.OnErrorHandler;
+    onResponse?: Config.Middlewares.OnResponseHandler;
+  }
+
+  export namespace Middlewares {
+    export type OnErrorHandler = (error: Config.Error) => void;
+    export type OnResponseHandler<T = any> = (data: T) => void;
+  }
 }
