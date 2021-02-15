5cbfa04b21bf82a403e490af4ad3d97856e4c086
Vladislav Tupikin <mrrefactoring@yandex.ru>
2021-02-15 23:02:34 +0300

    telemetry client added

--- src/clients/baseClient.ts
+++ src/clients/baseClient.ts
@@ -1,21 +1,25 @@
 import axios, { AxiosInstance } from 'axios';
+import { Telemetry, TelemetryClient } from 'telemetry.trello.js';
 import { Client } from './client';
 import { Callback, RequestConfig } from '../types';
 import { Config } from '../config';
 
 export class BaseClient implements Client {
   private instance: AxiosInstance;
+  private telemetryClient: TelemetryClient;
 
   constructor(private config: Config) {
+    this.telemetryClient = new TelemetryClient(config.telemetry);
     this.instance = axios.create({
       ...config.baseRequestConfig,
       baseURL: 'https://api.trello.com/1',
     });
   }
 
-  async sendRequest<T>(requestConfig: RequestConfig, callback?: Callback<T> | undefined, telemetryData?: Partial<any>): Promise<T>;
-  async sendRequest<T>(requestConfig: RequestConfig, callback: Callback<T>, telemetryData?: Partial<any>): Promise<void>;
-  async sendRequest<T>(rawRequestConfig: RequestConfig, callback?: Callback<T>, telemetryData?: Partial<any>): Promise<T | void> {
+  async sendRequest<T>(requestConfig: RequestConfig, callback?: Callback<T> | undefined, telemetryData?: Partial<Telemetry>): Promise<T>;
+  async sendRequest<T>(requestConfig: RequestConfig, callback: Callback<T>, telemetryData?: Partial<Telemetry>): Promise<void>;
+  async sendRequest<T>(rawRequestConfig: RequestConfig, callback?: Callback<T>, telemetryData?: Partial<Telemetry>): Promise<T | void> {
+    const startDateTime = new Date();
     const requestConfig: RequestConfig = {
       ...rawRequestConfig,
       params: {
@@ -23,6 +27,23 @@ export class BaseClient implements Client {
         key: this.config.apiKey,
         token: this.config.apiToken,
       },
+    };
+
+    const telemetry: Telemetry = {
+      authenticated: !!(this.config.apiKey && this.config.apiToken),
+      bodyExists: !!requestConfig.data,
+      callbackUsed: !!callback,
+      libVersion: '1.0.0',
+      libVersionHash: '226a631aedb85a43e7ad5714c4e623b5',
+      methodName: telemetryData?.methodName || 'sendRequest',
+      onErrorMiddlewareUser: !!this.config.middlewares?.onError,
+      onResponseMiddlewareUsed: !!this.config.middlewares?.onResponse,
+      queryExists: !!requestConfig.params,
+      requestStartTime: startDateTime,
+      requestEndTime: new Date(),
+      requestStatusCode: 0,
+      timeout: this.config.baseRequestConfig?.timeout || this.instance.defaults.timeout || -1,
+      ...telemetryData,
     };
 
     try {
@@ -35,6 +56,8 @@ export class BaseClient implements Client {
 
       this.config.middlewares?.onResponse?.(response.data);
 
+      telemetry.requestStatusCode = response.status;
+
       return responseHandler(response.data);
     } catch (e) {
       const callbackErrorHandler = callback && ((error: Config.Error) => callback(error));
@@ -46,9 +69,13 @@ export class BaseClient implements Client {
 
       this.config.middlewares?.onError?.(e);
 
+      telemetry.requestStatusCode = e.status;
+
       return errorHandler(e);
     } finally {
-      console.log(telemetryData);
+      telemetry.requestEndTime = new Date();
+
+      this.telemetryClient.sendTelemetry(telemetry);
     }
   }
 }
--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,18 @@
 import { AxiosError, AxiosRequestConfig } from 'axios';
+import { TelemetryConfig } from 'telemetry.trello.js';
 
 export interface Config {
   apiKey: string;
   apiToken: string;
   baseRequestConfig?: Config.BaseRequestConfig;
   middlewares?: Config.Middlewares;
+  telemetry?: Config.Telemetry;
 }
 
 export namespace Config {
   export type Error = AxiosError;
   export type BaseRequestConfig = AxiosRequestConfig;
+  export type Telemetry = boolean | TelemetryConfig;
 
   export interface Middlewares {
     onError?: Config.Middlewares.OnErrorHandler;
