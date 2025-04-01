25ea5e2a2df8f1c9285fd26aa611b0786a577515
FiliRezGelly <145478965+FiliRezGelly@users.noreply.github.com>
2025-04-01 17:52:38 +0200

    Update customField.ts
    display object property contains only cardFront property. All other properties should be outside display object property.

--- src/api/models/customField.ts
+++ src/api/models/customField.ts
@@ -7,17 +7,17 @@ export interface CustomField {
   fieldGroup?: string;
   display?: {
     cardFront?: boolean;
-    name?: string;
-    pos?: string;
-    options?: {
-      id?: TrelloID;
-      idCustomField?: TrelloID;
-      value?: {
-        text?: string;
-      };
-      color?: string;
-      pos?: number;
-    }[];
   };
+  name?: string;
+  pos?: string;
+  options?: {
+    id?: TrelloID;
+    idCustomField?: TrelloID;
+    value?: {
+      text?: string;
+    };
+    color?: string;
+    pos?: number;
+  }[];
   type?: string;
 }
